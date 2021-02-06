23a946fa7fa852d92ef698c2b33e929bfa573b8c
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2021-02-06 10:14:50 -0500

    fix is valid email method

--- MMS_CapstoneProject/MainForm.cs
+++ MMS_CapstoneProject/MainForm.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -59,7 +61,7 @@ namespace MMS_CapstoneProject
             {
                 errorMessage += "Please fill out primary contact email\n";
             }
-            else if (IsValidEmail(primaryContactEmail))
+            else if (!IsValidEmail(primaryContactEmail))
             {
                 errorMessage += primaryContactEmail + " is not a valid email\n";
             }
@@ -190,18 +192,50 @@ namespace MMS_CapstoneProject
             txtPrimaryContactEmail.Clear();
         }
 
-        private bool IsValidEmail(string email)
+        public static bool IsValidEmail(string email)
         {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
             try
             {
-                var addr = new System.Net.Mail.MailAddress(email);
-                return addr.Address == email;
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
             }
-            catch
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
             {
                 return false;
             }
         }
+
 
     }
 }
