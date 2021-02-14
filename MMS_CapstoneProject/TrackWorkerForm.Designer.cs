08a0a117e0f789d1f8ca0c8ff905b67bf68d23e5
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2021-02-14 17:21:31 -0500

    add some label, textbox, radio buttons, and button for track worker form

--- MMS_CapstoneProject/TrackWorkerForm.Designer.cs
+++ MMS_CapstoneProject/TrackWorkerForm.Designer.cs
@@ -29,12 +29,264 @@ namespace MMS_CapstoneProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTrackWorkerId = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblTrackWorkerFirstName = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblTrackWorkerLastName = new System.Windows.Forms.Label();
+            this.lblTrackWorkerCell = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblClientWorkerEmail = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblTrackWorkerIsCapableCaptain = new System.Windows.Forms.Label();
+            this.gbIsCapableCaptain = new System.Windows.Forms.GroupBox();
+            this.rdioIsCapableCaptain_False = new System.Windows.Forms.RadioButton();
+            this.rdioIsCapableCaptain_True = new System.Windows.Forms.RadioButton();
+            this.gbIsDeleted = new System.Windows.Forms.GroupBox();
+            this.rdoIsDeleted_False = new System.Windows.Forms.RadioButton();
+            this.rdoIsDeleted_True = new System.Windows.Forms.RadioButton();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnEscape = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.gbIsCapableCaptain.SuspendLayout();
+            this.gbIsDeleted.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTrackWorkerId
+            // 
+            this.lblTrackWorkerId.AutoSize = true;
+            this.lblTrackWorkerId.Location = new System.Drawing.Point(76, 25);
+            this.lblTrackWorkerId.Name = "lblTrackWorkerId";
+            this.lblTrackWorkerId.Size = new System.Drawing.Size(21, 13);
+            this.lblTrackWorkerId.TabIndex = 0;
+            this.lblTrackWorkerId.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblTrackWorkerFirstName
+            // 
+            this.lblTrackWorkerFirstName.AutoSize = true;
+            this.lblTrackWorkerFirstName.Location = new System.Drawing.Point(76, 58);
+            this.lblTrackWorkerFirstName.Name = "lblTrackWorkerFirstName";
+            this.lblTrackWorkerFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lblTrackWorkerFirstName.TabIndex = 2;
+            this.lblTrackWorkerFirstName.Text = "First Name:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(159, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // lblTrackWorkerLastName
+            // 
+            this.lblTrackWorkerLastName.AutoSize = true;
+            this.lblTrackWorkerLastName.Location = new System.Drawing.Point(76, 101);
+            this.lblTrackWorkerLastName.Name = "lblTrackWorkerLastName";
+            this.lblTrackWorkerLastName.Size = new System.Drawing.Size(61, 13);
+            this.lblTrackWorkerLastName.TabIndex = 4;
+            this.lblTrackWorkerLastName.Text = "Last Name:";
+            // 
+            // lblTrackWorkerCell
+            // 
+            this.lblTrackWorkerCell.AutoSize = true;
+            this.lblTrackWorkerCell.Location = new System.Drawing.Point(79, 136);
+            this.lblTrackWorkerCell.Name = "lblTrackWorkerCell";
+            this.lblTrackWorkerCell.Size = new System.Drawing.Size(27, 13);
+            this.lblTrackWorkerCell.TabIndex = 6;
+            this.lblTrackWorkerCell.Text = "Cell:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(159, 128);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 7;
+            // 
+            // lblClientWorkerEmail
+            // 
+            this.lblClientWorkerEmail.AutoSize = true;
+            this.lblClientWorkerEmail.Location = new System.Drawing.Point(82, 166);
+            this.lblClientWorkerEmail.Name = "lblClientWorkerEmail";
+            this.lblClientWorkerEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblClientWorkerEmail.TabIndex = 8;
+            this.lblClientWorkerEmail.Text = "Email:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(159, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // lblTrackWorkerIsCapableCaptain
+            // 
+            this.lblTrackWorkerIsCapableCaptain.AutoSize = true;
+            this.lblTrackWorkerIsCapableCaptain.Location = new System.Drawing.Point(65, 195);
+            this.lblTrackWorkerIsCapableCaptain.Name = "lblTrackWorkerIsCapableCaptain";
+            this.lblTrackWorkerIsCapableCaptain.Size = new System.Drawing.Size(88, 13);
+            this.lblTrackWorkerIsCapableCaptain.TabIndex = 10;
+            this.lblTrackWorkerIsCapableCaptain.Text = "Capable Captain:";
+            // 
+            // gbIsCapableCaptain
+            // 
+            this.gbIsCapableCaptain.Controls.Add(this.rdioIsCapableCaptain_False);
+            this.gbIsCapableCaptain.Controls.Add(this.rdioIsCapableCaptain_True);
+            this.gbIsCapableCaptain.Location = new System.Drawing.Point(159, 184);
+            this.gbIsCapableCaptain.Name = "gbIsCapableCaptain";
+            this.gbIsCapableCaptain.Size = new System.Drawing.Size(195, 52);
+            this.gbIsCapableCaptain.TabIndex = 11;
+            this.gbIsCapableCaptain.TabStop = false;
+            // 
+            // rdioIsCapableCaptain_False
+            // 
+            this.rdioIsCapableCaptain_False.AutoSize = true;
+            this.rdioIsCapableCaptain_False.Location = new System.Drawing.Point(103, 23);
+            this.rdioIsCapableCaptain_False.Name = "rdioIsCapableCaptain_False";
+            this.rdioIsCapableCaptain_False.Size = new System.Drawing.Size(50, 17);
+            this.rdioIsCapableCaptain_False.TabIndex = 1;
+            this.rdioIsCapableCaptain_False.Text = "False";
+            this.rdioIsCapableCaptain_False.UseVisualStyleBackColor = true;
+            // 
+            // rdioIsCapableCaptain_True
+            // 
+            this.rdioIsCapableCaptain_True.AutoSize = true;
+            this.rdioIsCapableCaptain_True.Checked = true;
+            this.rdioIsCapableCaptain_True.Location = new System.Drawing.Point(12, 23);
+            this.rdioIsCapableCaptain_True.Name = "rdioIsCapableCaptain_True";
+            this.rdioIsCapableCaptain_True.Size = new System.Drawing.Size(47, 17);
+            this.rdioIsCapableCaptain_True.TabIndex = 0;
+            this.rdioIsCapableCaptain_True.TabStop = true;
+            this.rdioIsCapableCaptain_True.Text = "True";
+            this.rdioIsCapableCaptain_True.UseVisualStyleBackColor = true;
+            // 
+            // gbIsDeleted
+            // 
+            this.gbIsDeleted.Controls.Add(this.rdoIsDeleted_False);
+            this.gbIsDeleted.Controls.Add(this.rdoIsDeleted_True);
+            this.gbIsDeleted.Location = new System.Drawing.Point(140, 242);
+            this.gbIsDeleted.Name = "gbIsDeleted";
+            this.gbIsDeleted.Size = new System.Drawing.Size(200, 100);
+            this.gbIsDeleted.TabIndex = 12;
+            this.gbIsDeleted.TabStop = false;
+            // 
+            // rdoIsDeleted_False
+            // 
+            this.rdoIsDeleted_False.AutoSize = true;
+            this.rdoIsDeleted_False.Location = new System.Drawing.Point(58, 54);
+            this.rdoIsDeleted_False.Name = "rdoIsDeleted_False";
+            this.rdoIsDeleted_False.Size = new System.Drawing.Size(50, 17);
+            this.rdoIsDeleted_False.TabIndex = 1;
+            this.rdoIsDeleted_False.Text = "False";
+            this.rdoIsDeleted_False.UseVisualStyleBackColor = true;
+            // 
+            // rdoIsDeleted_True
+            // 
+            this.rdoIsDeleted_True.AutoSize = true;
+            this.rdoIsDeleted_True.Checked = true;
+            this.rdoIsDeleted_True.Location = new System.Drawing.Point(58, 30);
+            this.rdoIsDeleted_True.Name = "rdoIsDeleted_True";
+            this.rdoIsDeleted_True.Size = new System.Drawing.Size(47, 17);
+            this.rdoIsDeleted_True.TabIndex = 0;
+            this.rdoIsDeleted_True.TabStop = true;
+            this.rdoIsDeleted_True.Text = "True";
+            this.rdoIsDeleted_True.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(85, 348);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 13;
+            this.btnEnter.Text = "Create";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // btnEscape
+            // 
+            this.btnEscape.Location = new System.Drawing.Point(262, 348);
+            this.btnEscape.Name = "btnEscape";
+            this.btnEscape.Size = new System.Drawing.Size(75, 23);
+            this.btnEscape.TabIndex = 15;
+            this.btnEscape.Text = "Cancel";
+            this.btnEscape.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(171, 348);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // TrackWorkerForm
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEscape);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.gbIsDeleted);
+            this.Controls.Add(this.gbIsCapableCaptain);
+            this.Controls.Add(this.lblTrackWorkerIsCapableCaptain);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.lblClientWorkerEmail);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblTrackWorkerCell);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblTrackWorkerLastName);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblTrackWorkerFirstName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTrackWorkerId);
+            this.Name = "TrackWorkerForm";
             this.Text = "TrackWorkerForm";
+            this.gbIsCapableCaptain.ResumeLayout(false);
+            this.gbIsCapableCaptain.PerformLayout();
+            this.gbIsDeleted.ResumeLayout(false);
+            this.gbIsDeleted.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTrackWorkerId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblTrackWorkerFirstName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblTrackWorkerLastName;
+        private System.Windows.Forms.Label lblTrackWorkerCell;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label lblClientWorkerEmail;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblTrackWorkerIsCapableCaptain;
+        private System.Windows.Forms.GroupBox gbIsCapableCaptain;
+        private System.Windows.Forms.RadioButton rdioIsCapableCaptain_False;
+        private System.Windows.Forms.RadioButton rdioIsCapableCaptain_True;
+        private System.Windows.Forms.GroupBox gbIsDeleted;
+        private System.Windows.Forms.RadioButton rdoIsDeleted_False;
+        private System.Windows.Forms.RadioButton rdoIsDeleted_True;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnEscape;
+        private System.Windows.Forms.Button btnClear;
     }
 }
