5782b07ead1183163a11a90a6250b262f5f8c805
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2021-02-14 17:23:08 -0500

    add blank client event form

---
+++ MMS_CapstoneProject/ClientEventForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MMS_CapstoneProject
+{
+    public partial class ClientEventForm : Form
+    {
+        public ClientEventForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
