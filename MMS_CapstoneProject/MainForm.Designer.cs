12b27e2f6ab73f8bfa3b047b6cc74b87e95fb825
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2021-02-16 10:38:26 -0500

    first draft for main form

---
+++ MMS_CapstoneProject/MainForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace MMS_CapstoneProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabClient = new System.Windows.Forms.TabPage();
+            this.tabTrackWorker = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClientAddNew = new System.Windows.Forms.Button();
+            this.txtClientSearch = new System.Windows.Forms.TextBox();
+            this.btnClientSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabTrack = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabClientEvent = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabClient.SuspendLayout();
+            this.tabTrackWorker.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabTrack.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabClientEvent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabClient);
+            this.tabControl1.Controls.Add(this.tabTrackWorker);
+            this.tabControl1.Controls.Add(this.tabTrack);
+            this.tabControl1.Controls.Add(this.tabClientEvent);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabClient
+            // 
+            this.tabClient.Controls.Add(this.btnClientSearch);
+            this.tabClient.Controls.Add(this.txtClientSearch);
+            this.tabClient.Controls.Add(this.btnClientAddNew);
+            this.tabClient.Controls.Add(this.dataGridView1);
+            this.tabClient.Location = new System.Drawing.Point(4, 22);
+            this.tabClient.Name = "tabClient";
+            this.tabClient.Padding = new System.Windows.Forms.Padding(3);
+            this.tabClient.Size = new System.Drawing.Size(768, 400);
+            this.tabClient.TabIndex = 0;
+            this.tabClient.Text = "Client";
+            this.tabClient.UseVisualStyleBackColor = true;
+            // 
+            // tabTrackWorker
+            // 
+            this.tabTrackWorker.Controls.Add(this.button1);
+            this.tabTrackWorker.Controls.Add(this.textBox1);
+            this.tabTrackWorker.Controls.Add(this.button2);
+            this.tabTrackWorker.Controls.Add(this.dataGridView2);
+            this.tabTrackWorker.Location = new System.Drawing.Point(4, 22);
+            this.tabTrackWorker.Name = "tabTrackWorker";
+            this.tabTrackWorker.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTrackWorker.Size = new System.Drawing.Size(768, 400);
+            this.tabTrackWorker.TabIndex = 1;
+            this.tabTrackWorker.Text = "Track Worker";
+            this.tabTrackWorker.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(762, 331);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnClientAddNew
+            // 
+            this.btnClientAddNew.Location = new System.Drawing.Point(6, 12);
+            this.btnClientAddNew.Name = "btnClientAddNew";
+            this.btnClientAddNew.Size = new System.Drawing.Size(100, 45);
+            this.btnClientAddNew.TabIndex = 1;
+            this.btnClientAddNew.Text = "Add New";
+            this.btnClientAddNew.UseVisualStyleBackColor = true;
+            // 
+            // txtClientSearch
+            // 
+            this.txtClientSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClientSearch.Location = new System.Drawing.Point(270, 16);
+            this.txtClientSearch.Name = "txtClientSearch";
+            this.txtClientSearch.Size = new System.Drawing.Size(140, 29);
+            this.txtClientSearch.TabIndex = 2;
+            // 
+            // btnClientSearch
+            // 
+            this.btnClientSearch.Location = new System.Drawing.Point(416, 12);
+            this.btnClientSearch.Name = "btnClientSearch";
+            this.btnClientSearch.Size = new System.Drawing.Size(100, 45);
+            this.btnClientSearch.TabIndex = 3;
+            this.btnClientSearch.Text = "Search";
+            this.btnClientSearch.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 45);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(273, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 29);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add New";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 60);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(762, 331);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // tabTrack
+            // 
+            this.tabTrack.Controls.Add(this.button3);
+            this.tabTrack.Controls.Add(this.textBox2);
+            this.tabTrack.Controls.Add(this.button4);
+            this.tabTrack.Controls.Add(this.dataGridView3);
+            this.tabTrack.Location = new System.Drawing.Point(4, 22);
+            this.tabTrack.Name = "tabTrack";
+            this.tabTrack.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTrack.Size = new System.Drawing.Size(768, 400);
+            this.tabTrack.TabIndex = 2;
+            this.tabTrack.Text = "Track";
+            this.tabTrack.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 45);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(273, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 45);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Add New";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 60);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(762, 331);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // tabClientEvent
+            // 
+            this.tabClientEvent.Controls.Add(this.button5);
+            this.tabClientEvent.Controls.Add(this.textBox3);
+            this.tabClientEvent.Controls.Add(this.button6);
+            this.tabClientEvent.Controls.Add(this.dataGridView4);
+            this.tabClientEvent.Location = new System.Drawing.Point(4, 22);
+            this.tabClientEvent.Name = "tabClientEvent";
+            this.tabClientEvent.Padding = new System.Windows.Forms.Padding(3);
+            this.tabClientEvent.Size = new System.Drawing.Size(768, 400);
+            this.tabClientEvent.TabIndex = 3;
+            this.tabClientEvent.Text = "Client Event";
+            this.tabClientEvent.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(419, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 45);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Search";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(273, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 29);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(9, 9);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 45);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Add New";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(3, 60);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(762, 331);
+            this.dataGridView4.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.tabControl1.ResumeLayout(false);
+            this.tabClient.ResumeLayout(false);
+            this.tabClient.PerformLayout();
+            this.tabTrackWorker.ResumeLayout(false);
+            this.tabTrackWorker.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabTrack.ResumeLayout(false);
+            this.tabTrack.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabClientEvent.ResumeLayout(false);
+            this.tabClientEvent.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabClient;
+        private System.Windows.Forms.TabPage tabTrackWorker;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClientSearch;
+        private System.Windows.Forms.TextBox txtClientSearch;
+        private System.Windows.Forms.Button btnClientAddNew;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabTrack;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TabPage tabClientEvent;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView4;
+    }
+}
